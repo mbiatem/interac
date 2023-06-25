02913c5272222d97f7523bea062f48574ca4f5e9
Armstrong Elorh <el.amy2019@gmail.com>
2023-06-25 11:38:23 -0400

    vpc created

--- main.tf
+++ main.tf
@@ -1,3 +1,13 @@
 provider "aws" {
-    
 }
+
+resource "aws_vpc" "CloudPlus" {
+  cidr_block = "10.1.0.0/16"
+  enable_dns_hostnames = true
+  enable_dns_support = true
+  tags = {
+    "Name" = "DEV01" 
+  }
+}
+
+
